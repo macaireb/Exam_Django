4e1eb573e90822d0e5237f779d4f913e05327b0d
macaireb <macairealexander@gmail.com>
2022-01-07 20:25:15 -0500

    Added an actual edit_profile option and customized registration fields, change password should be working

--- members/urls.py
+++ members/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/', PasswordsChangeView.as_view()),
 ]
--- members/views.py
+++ members/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
+from .forms import *
 
 # Create your views here.
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangingForm
+    template_name = 'registration/change_password.html'
+    success_url = reverse_lazy('home')
 
